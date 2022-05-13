5d9621c703167abca925414378e60188a1da3f91
grantblackbean <grant@blackbeancreative.com>
2022-05-13 14:16:19 -0700

    test5

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -5,4 +5,16 @@
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+  sad_lowcase = string.scan("sad")
+  sad_upcase = string.scan("Sad")
+  sad_lowcase.each do
+    string.sub!("sad", "happy")
+  end
+  sad_upcase.each do
+    string.sub!("Sad", "Happy")
+  end
+  puts string
+  return string
 end
+
+add_more_ruby("Sad, sad, sad times are ahead!")
